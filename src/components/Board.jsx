748b08a7d6fc39f6b4350961be44b63ec3d4867a
Hien Hoang <hien.hoang@tpptechnology.com>
2021-03-12 11:26:29 +0700

    feat: Add take to slice redundant board string

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Square from './Square';
-import { pad, chunk, map } from 'lodash';
+import { pad, chunk, map, take } from 'lodash';
 
 import './Board.css'
 
 const Board = ({ value = pad('', 9, ' ') }) => (
   <div className="board">
-    {map(chunk(value, 3), row => (
+    {map(chunk(take(value, 9), 3), row => (
       <div className="row">
         {map(row, cell => (
           <Square value={cell} />
